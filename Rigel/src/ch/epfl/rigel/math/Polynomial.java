30249949bf71a1aaeca0671187e0afe36f6a70e7
Ghemizz <ghemizzdd@gmail.com>
2020-02-17 20:50:41 +0100

    Fix Polynomial

--- Rigel/src/ch/epfl/rigel/math/Polynomial.java
+++ Rigel/src/ch/epfl/rigel/math/Polynomial.java
@@ -1,4 +1,7 @@
 package ch.epfl.rigel.math;
+import static ch.epfl.rigel.Preconditions.checkArgument;
+
+import java.util.Locale;
 
 public final class Polynomial {
 	private double[] coefficients;
@@ -11,27 +14,48 @@ public final class Polynomial {
 		System.arraycopy(temp, 0, coefficients, 1, temp.length);
 	}
 	
-	public static Polynomial of(double coefficientN, double[] coefficients) {
+	public static Polynomial of(double coefficientN, double... coefficients) {
+		checkArgument(coefficientN!=0);
 		return new Polynomial(coefficientN, coefficients);
 	}
 	
 	public String toString() {
-		int count=coefficients.length;
+		int count=coefficients.length-1;
 		StringBuilder string=new StringBuilder();
 		
 		for(int i=0; i<coefficients.length; ++i) {
 			if (coefficients[i]!=0) {
-				string.append(coefficients[i]+" ");
-				if (count!=0)
+				string.append(coefficients[i]);
+				if (count!=0) {
 					string.append("x^"+count);
-				
+					string.append("+");
+				}
+					
 				else
 					string.append(count);
+				
+				
 			}
 			--count;
 		}
 	
 		return string.toString();	
+	}
+	
+	public double at(double v) {
+		int count=coefficients.length-1;
+		double poly=0.0;
+		double pow=1.0;
+		
+		for(int i=0; i<coefficients.length; ++i) {
+			pow=1.0;
+			for(int y=0; y<count; ++y) {
+				pow*=v;
+			}
+			poly+=pow*coefficients[i];
+			--count;
+		}
+		return poly;
 	}
 	
 	@Override
