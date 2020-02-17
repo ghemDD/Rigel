8f9e97b5f9c60c01b8aa8d3ca38a5057ac62bf26
Ghemizz <ghemizzdd@gmail.com>
2020-02-17 19:41:54 +0100

    Fix Angle

--- Rigel/src/ch/epfl/rigel/math/Angle.java
+++ Rigel/src/ch/epfl/rigel/math/Angle.java
@@ -2,16 +2,14 @@ package ch.epfl.rigel.math;
 import static ch.epfl.rigel.Preconditions.checkArgument;
 
 public final class Angle {
-	//ChangesMOMO
+
 	public final static double TAU=2*Math.PI;
 	private static final double DEG_PER_RAD = 360.0 / TAU;
-	private static final double RAD_PER_DEG= TAU/360.0;
+	private static final double RAD_PER_DEG= TAU / 360.0;
 	
-	 
-	
-	public final static double RAD_PER_HR=Math.toRadians(15.0);
-	public final static double RAD_PER_MIN=Math.toRadians(15.0/60.0);
-	public final static double RAD_PER_SEC=Math.toRadians(15.0/3600.0);
+	public final static double RAD_PER_HR=ofDeg(15.0);
+	public final static double RAD_PER_MIN=ofDeg(1.0/60.0);
+	public final static double RAD_PER_SEC=ofDeg(1.0/3600.0);
 	
 	
 	public static double normalizePositive(double rad) {
@@ -20,13 +18,13 @@ public final class Angle {
 	}
 	
 	public static double ofArcsec(double sec) {
-		return (sec*RAD_PER_SEC);
+		return sec*RAD_PER_SEC;
 	}
 	
 	public static double ofDMS(int deg, int min, double sec) {
 		checkArgument((0<=min && min<60) && (0<=sec && sec<60));
 		
-		return ofDeg(deg)+min*RAD_PER_MIN+sec*RAD_PER_SEC;
+		return ofDeg(deg)+min*RAD_PER_MIN+ofArcsec(sec);
 	}
 	
 	public static double ofDeg(double deg) {
